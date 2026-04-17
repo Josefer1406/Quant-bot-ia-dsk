7a904a06d371abd18985bc247e08da7398957f7e
Josefer1406 <miler126@gmail.com>
2026-04-16 20:04:30 -0500

    Update exchange_connector.py

--- exchange_connector.py
+++ exchange_connector.py
@@ -3,21 +3,33 @@ import config
 
 class ExchangeConnector:
     def __init__(self):
-        if config.EXCHANGE_NAME == "okx":
-            self.exchange = ccxt.okx({
-                'apiKey': config.OKX_API_KEY,
-                'secret': config.OKX_SECRET_KEY,
-                'password': config.OKX_PASSPHRASE,
-                'enableRateLimit': True,
-            })
-        elif config.EXCHANGE_NAME == "binance":
-            self.exchange = ccxt.binance({
-                'apiKey': config.BINANCE_API_KEY,
-                'secret': config.BINANCE_SECRET_KEY,
-                'enableRateLimit': True,
-            })
+        if config.SIMULATION_MODE:
+            if config.EXCHANGE_NAME == "okx":
+                self.exchange = ccxt.okx({
+                    'enableRateLimit': True,
+                })
+            elif config.EXCHANGE_NAME == "binance":
+                self.exchange = ccxt.binance({
+                    'enableRateLimit': True,
+                })
+            else:
+                raise ValueError("Exchange desconocido")
         else:
-            raise ValueError("Exchange desconocido")
+            if config.EXCHANGE_NAME == "okx":
+                self.exchange = ccxt.okx({
+                    'apiKey': config.OKX_API_KEY,
+                    'secret': config.OKX_SECRET_KEY,
+                    'password': config.OKX_PASSPHRASE,
+                    'enableRateLimit': True,
+                })
+            elif config.EXCHANGE_NAME == "binance":
+                self.exchange = ccxt.binance({
+                    'apiKey': config.BINANCE_API_KEY,
+                    'secret': config.BINANCE_SECRET_KEY,
+                    'enableRateLimit': True,
+                })
+            else:
+                raise ValueError("Exchange desconocido")
     
     def fetch_ohlcv(self, symbol, timeframe, limit):
         return self.exchange.fetch_ohlcv(symbol, timeframe, limit=limit)
